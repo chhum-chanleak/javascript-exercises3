4fbc2792ced8a84bc53dc60299e6b7f1098d1928
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-10 17:42:48 +0700

    feat: Implment reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let convertedString = string.split("");
+  let newString = [];
 
+  for (let i = 0; i < convertedString.length; i += 1) {
+    newString.push(convertedString.pop());
+    i -= 1;
+  }
+
+  return newString.join("");
 };
 
 // Do not edit below this line
