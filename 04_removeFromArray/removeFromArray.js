d959af3abcbf5ac11f1e7ca414760bcd88714154
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-10 18:06:51 +0700

    feat: Implement removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < items.length; j += 1) {
+      if (array[i] === items[j]){
+        array.splice(i, 1);
+        i -= 1;
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
